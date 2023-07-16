105439f07d986666d03fb6ef069442c0a62507f2
Chris Henk <cahenk95@gmail.com>
2023-07-16 12:45:06 -0700

    checkpoint

--- src/self_updating_task.rs
+++ src/self_updating_task.rs
@@ -1,22 +1,36 @@
-use std::sync::Arc;
+use std::cell::Cell;
+use std::num::TryFromIntError;
 use std::time::Duration;
 use crate::PollingTask;
 
 pub struct SelfUpdatingPollingTask {
-    polling_task: PollingTask
+    polling_task: Option<PollingTask>
 }
 
-pub type PollingIntervalSetter = dyn Fn(Duration);
+pub type PollingIntervalSetter = dyn FnMut(Duration) -> Result<(), TryFromIntError>;
 
-// impl SelfUpdatingPollingTask {
-//     pub fn new(interval: Duration, task: impl Fn(&PollingIntervalSetter)) -> Self {
-//         let test: Arc<Self>;
-//         let test2 = test.clone();
-//
-//         let wrapper = move |duration: Duration| test2.polling_task.set_polling_rate(duration);
-//
-//         Self {
-//             polling_task: PollingTask::new(interval, move || task(&wrapper))
-//         }
-//     }
-// }
+impl SelfUpdatingPollingTask {
+    pub fn new(interval: Duration, task: Box<dyn Fn(&PollingIntervalSetter) + Send>) -> Result<Self, TryFromIntError> {
+        let outer_task = SelfUpdatingPollingTask{ polling_task: None };
+        let mut cell = Cell::new(outer_task);
+
+        let setter = move |duration: Duration| cell.get_mut().polling_task.as_ref().unwrap().set_polling_rate(duration);
+        let wrapper = move || (*task)(&setter);
+
+        Ok(Self {
+            polling_task: Some(PollingTask::new(interval, Box::new(wrapper))?)
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::atomic::Ordering::SeqCst;
+    use std::thread::sleep;
+    use super::*;
+
+    #[test]
+    fn update_observed_on_next_poll() {
+
+    }
+}
--- src/task.rs
+++ src/task.rs
@@ -1,3 +1,4 @@
+use std::num::TryFromIntError;
 use std::sync::atomic::AtomicU64;
 use std::sync::atomic::Ordering::Relaxed;
 use std::sync::{Arc, Condvar, Mutex};
@@ -16,12 +17,12 @@ pub struct PollingTask {
 
 impl PollingTask {
     /// The interval must be expressible as a u64 in milliseconds.
-    pub fn new(interval: Duration, task: Box<dyn Fn() + Send>) -> Self {
+    pub fn new(interval: Duration, task: Box<dyn Fn() + Send>) -> Result<Self, TryFromIntError> {
         let polling_task = Self {
             shared_state: Arc::new(InnerState {
                 active: Mutex::new(true),
                 signal: Condvar::new(),
-                interval: AtomicU64::new(u64::try_from(interval.as_millis()).unwrap()),
+                interval: AtomicU64::new(u64::try_from(interval.as_millis())?),
             }),
         };
 
@@ -31,11 +32,12 @@ impl PollingTask {
             Self::poll(&shared_state, &task);
         });
 
-        polling_task
+        Ok(polling_task)
     }
 
-    pub fn set_polling_rate(&mut self, interval: Duration) {
-        self.shared_state.interval.store(u64::try_from(interval.as_millis()).unwrap(), Relaxed);
+    pub fn set_polling_rate(&self, interval: Duration) -> Result<(), TryFromIntError> {
+        self.shared_state.interval.store(u64::try_from(interval.as_millis())?, Relaxed);
+        Ok(())
     }
 
     fn poll(shared_state: &Arc<InnerState>, task: &Box<dyn Fn() + Send>) {
@@ -62,5 +64,46 @@ impl Drop for PollingTask {
     fn drop(&mut self) {
         *self.shared_state.active.lock().unwrap() = false;
         self.shared_state.signal.notify_one();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::atomic::Ordering::SeqCst;
+    use std::thread::sleep;
+    use super::*;
+
+    #[test]
+    fn poll() {
+        let (counter, _task) = get_task_and_timer();
+        sleep(Duration::from_millis(50));
+
+        assert!(counter.load(SeqCst) > 1);
+    }
+
+    fn get_task_and_timer() -> (Arc<AtomicU64>, PollingTask) {
+        let counter = Arc::new(AtomicU64::new(0));
+        let counter_clone = counter.clone();
+
+        let task = PollingTask::new(Duration::from_millis(1), Box::new(move || { counter_clone.fetch_add(1, SeqCst); }));
+        (counter, task.unwrap())
+    }
+
+    #[test]
+    fn set_polling_rate() {
+        let (counter, task) = get_task_and_timer();
+
+        // Wait for task to actually run
+        sleep(Duration::from_millis(50));
+
+        task.set_polling_rate(Duration::from_secs(5)).unwrap();
+
+        // Wait for effect
+        sleep(Duration::from_millis(50));
+
+        // Observe no change in counter
+        let counter_value = counter.load(SeqCst);
+        sleep(Duration::from_millis(50));
+        assert_eq!(counter_value, counter.load(SeqCst))
     }
 }
