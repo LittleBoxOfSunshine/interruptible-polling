f171f0eba34d9f38068e0b511e706dc1f72e1632
Chris Henk <chris.henk@unobtainium.software>
2024-01-17 21:59:21 -0800

    chaos

--- src/fire_and_forget.rs
+++ src/fire_and_forget.rs
@@ -1,6 +1,10 @@
+use std::cell::{RefCell, UnsafeCell};
+use std::pin::Pin;
+use std::rc::Rc;
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
-use crate::UnitTask;
+use crate::{PollingIntervalSetter, UnitTask};
 
 pub fn fire_and_forget_polling_task<F>(interval: Duration, task: F)
     where
@@ -13,6 +17,52 @@ pub fn fire_and_forget_polling_task<F>(interval: Duration, task: F)
             thread::sleep(interval);
         }
     });
+}
+
+struct Wrapper {
+    ptr: *mut u64,
+    wut: i64
+}
+
+struct Wrapper2 {
+    f: Box<dyn Fn(Duration)>
+}
+
+unsafe impl Send for Wrapper{}
+unsafe impl Send for Wrapper2{}
+
+
+pub fn self_updating_fire_and_forget_polling_task<F>(interval: Duration, task: F)
+    where
+        F: Fn(&dyn Fn(Duration)) + Send + 'static
+{
+    let mut interval = Box::new(interval.as_secs());
+    let ptr= &mut *interval as *mut u64;
+    let ptr = Wrapper{ptr, wut: 0};
+    //let test = UnsafeCell::new(interval);
+    //let mut ptr = Wrapper{ptr};
+    //let pin = Pin::new(interval);
+    //let interval = Arc::new(RefCell::new(interval));
+    //let mut interval_clone = interval.clone();
+    let setter = move |duration: Duration| unsafe {
+        let _ = ptr.wut;
+        *(ptr.ptr) = duration.as_secs();
+
+        //Ok(())
+    };
+
+    let setter = Wrapper2 {f: Box::new(setter) };
+
+        thread::spawn(move || unsafe {
+            let setter = setter;
+            loop {
+                task(&*setter.f);
+
+                //let test = &mut *interval as *mut u64;
+
+                thread::sleep(Duration::from_secs(*interval));
+            }
+        });
 }
 
 #[cfg(test)]
